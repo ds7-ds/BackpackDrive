9ea2f445b0269c63df5157a0f611da269c283eca
Darshan Shet <darshanshet777@gmail.com>
2022-01-12 08:14:46 -0600

    detects if USB is connected and displays status

--- script.sh
+++ script.sh
@@ -2,4 +2,14 @@
 
 # Auto backup "Darshan" folder in Documents to USB Flash backup drives if connected
 USER=$(whoami)
-echo $USER
+cd /media/$USER
+DRIVE_MOUNTED=$(ls)
+echo $DRIVE_MOUNTED
+DRIVE_A="BACKUP A"
+if [ "$DRIVE_MOUNTED"=="$DRIVE_A" ]
+then
+	echo "Backing up files to USB"
+else
+	echo "Cannot backup to USB"
+fi
+
