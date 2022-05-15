303c70e42020b22dae8a833c4ee9213cbe8bac85
Darshan Shet <dshet2@uic.edu>
2022-05-15 10:09:17 -0500

    cleaned output; fixed var naming

--- backup.sh
+++ backup.sh
@@ -14,34 +14,32 @@ FOLDER_FROM="darshan"
 FOLDER_TO="darshan"
 DRIVE_BACKUP="BACKUP A"
 cd /media/"$USER"/"$DRIVE_BACKUP"/"$FOLDER_TO" &> /dev/null
-DRIVE_MOUNTED=$?
+DRIVE_MOUNTED_STATUS=$?
 
 # If connected, backup files otherwise display an error message and quit
-if [ $DRIVE_MOUNTED == 0 ]
+if [ $DRIVE_MOUNTED_STATUS == 0 ]
 then
-	echo "Backing up files to USB"
-	exit
+	echo "Backing up files to: "$DRIVE_BACKUP"..."
 	# Go to Documents and obtain local folder modification date
 	cd ~/Documents
-	ls
-	TEMP_DATE_1=$(stat 'Darshan' | grep "Modify" | cut -c 9-27)
+	TEMP_DATE_1=$(stat $FOLDER_FROM | grep "Modify" | cut -c 9-27)
 	LOCAL_MOD_DATE=$(date -d "$TEMP_DATE_1" +%s)
-	echo $LOCAL_MOD_DATE
+	echo "Last modification to local folder "$FOLDER_FROM": "$TEMP_DATE_1
 	# Go to backup drive, get mod date, and compare dates
-	cd /media/"$USER"/"$DRIVE_MOUNTED"
-	ls
-	TEMP_DATE_2=$(stat 'Darshan' | grep "Modify" | cut -c 9-27)
+	cd /media/"$USER"/"$DRIVE_BACKUP"
+	TEMP_DATE_2=$(stat $FOLDER_TO | grep "Modify" | cut -c 9-27)
 	BACKUP_MOD_DATE=$(date -d "$TEMP_DATE_2" +%s)
-	echo $BACKUP_MOD_DATE
-	if [[ "$BACKUP_MOD_DATE" < "$LOCAL_MOD_DATE" ]]
+	echo "Last modification to backup folder "$FOLDER_TO": "$TEMP_DATE_2
+	if [[ $BACKUP_MOD_DATE < $LOCAL_MOD_DATE ]]
 	then
-		echo "Backup is old"
+		echo "Updating backup with newer local folder..."
+		echo ""
 		cd ~/Documents
-		rsync --verbose --recursive --update --delete-after $FOLDER_FROM/ /media/$USER/$DRIVE_MOUNTED/$FOLDER_TO
+		rsync --verbose --recursive --update --delete-after $FOLDER_FROM/ /media/$USER/$DRIVE_BACKUP/$FOLDER_TO
 	else
-		read -t 5 -p "Backup is current"
+		echo "Backup is current. No update needed..."
 	fi
 else
-	echo "Cannot backup to USB"
+	echo "Cannot backup to"$DRIVE_BACKUP". No USB drive detected..."
 fi
 
