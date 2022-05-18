9f0aceef807d020b2f407f5566db949e675b8e02
Darshan Shet <dshet2@uic.edu>
2022-05-18 18:48:12 -0500

    fixed issue of untracked new and deleted files in local folders

--- backup.sh
+++ backup.sh
@@ -15,6 +15,7 @@
 # https://stackoverflow.com/questions/14922562/how-do-i-copy-folder-with-files-to-another-folder-in-unix-linux
 # https://stackoverflow.com/questions/4561895/how-to-recursively-find-the-latest-modified-file-in-a-directory
 # https://linuxize.com/post/linux-cut-command/
+# https://unix.stackexchange.com/questions/226310/using-file-date-time-as-metadata-reliable
 
 # Get local user to access folders
 USER=$(whoami)
@@ -27,17 +28,17 @@ cd /media/"$USER"/"$DRIVE_BACKUP"/"$FOLDER_TO" &> /dev/null
 DRIVE_MOUNTED_STATUS=$?
 
 # If connected, backup files otherwise display an error message and quit
-if [ $DRIVE_MOUNTED_STATUS == 0 ]
+if [[ $DRIVE_MOUNTED_STATUS == 0 ]]
 then
 	echo "Backing up files to: "$DRIVE_BACKUP"..."
 	# Go to Documents and obtain local folder modification date
 	cd ~/Documents
-	LOCAL_MOD_TIME=$(find $FOLDER_FROM/ -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f1 -d".")
+	LOCAL_MOD_TIME=$(find $FOLDER_FROM/ -type d -printf '%T@ %p\n' | sort -n | tail -1 | cut -f1 -d".")
 	LOCAL_MOD_DATE=$(date -d @$LOCAL_MOD_TIME)
 	echo "Last modification to local folder "$FOLDER_FROM": "$LOCAL_MOD_DATE
 	# Go to backup drive, get mod date, and compare dates
 	cd /media/"$USER"/"$DRIVE_BACKUP"
-	BACKUP_MOD_TIME=$(find $FOLDER_TO/ -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f1 -d".")
+	BACKUP_MOD_TIME=$(find $FOLDER_TO/ -type d -printf '%T@ %p\n' | sort -n | tail -1 | cut -f1 -d".")
 	BACKUP_MOD_DATE=$(date -d @$BACKUP_MOD_TIME)
 	echo "Last modification to backup folder "$FOLDER_TO": "$BACKUP_MOD_DATE
 	if [[ $BACKUP_MOD_DATE < $LOCAL_MOD_DATE ]]
@@ -52,4 +53,3 @@ then
 else
 	echo "Cannot backup to"$DRIVE_BACKUP". No USB drive detected..."
 fi
-
